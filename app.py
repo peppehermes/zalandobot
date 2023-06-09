ef9c996cb957a430e559e74efe072acea40e6f4b
Giuseppe Mercurio <giuseppemercurio1997@gmail.com>
2023-06-09 19:46:57 +0200

    Removed all updates from run_polling

--- app.py
+++ app.py
@@ -163,7 +163,7 @@ def main() -> None:
     application.add_handler(conv_handler)
 
     # Run the bot until the user presses Ctrl-C
-    application.run_polling(allowed_updates=Update.ALL_TYPES)
+    application.run_polling()
 
 
 if __name__ == "__main__":
