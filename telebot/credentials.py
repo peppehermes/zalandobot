2b534c325cbbe1cbaf73af7f2a3c83213b4d08d9
Giuseppe Mercurio <giuseppemercurio1997@gmail.com>
2023-06-09 13:23:00 +0200

    added URL

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "6081773665:AAFs2tUw1PBQcHDdWHpLOQjNkG9mriBwYjc"
 bot_user_name = "zalando_giuseppe_bot"
-URL = "the heroku app link that we will create later"
+URL = "https://zalando-bot-peppehermes.koyeb.app/"
